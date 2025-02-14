d06433c8f4bbd98dfae1c251eb2d1779ee5414bd
Bhush8766 <159047235+Bhush8766@users.noreply.github.com>
2025-02-14 08:49:04 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/contactDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27016/contactDB', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB...', err));
 
